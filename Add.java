014aee66dd9e79e8eefb70ba02a0992e781284bc
renuka <renukadevi29730@gmail.com>
2021-08-22 01:28:42 -0700

    multplication added

--- Add.java
+++ Add.java
@@ -8,12 +8,17 @@ public class Add {
     public static int sub(int firstNo, int secondNo){
         return firstNo-secondNo;
     }
+    public static int mult(int firstNo, int secondNo){
+        return firstNo*secondNo;
+    }
     public static void main(String args[])
     {   int firstNo=5,secondNo=3;
         int res=add(firstNo,secondNo);
         int res1=sub(firstNo,secondNo);
+        int res2=mult(firstNo,secondNo);
         System.out.println(res);
         System.out.println(res1);
+        System.out.println(res2);
 
     }
 }
