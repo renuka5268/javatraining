e79744b8d63f7d1b695ef1e4b3ce98a82cc14e8d
renuka <renukadevi29730@gmail.com>
2021-08-22 01:09:28 -0700

    subtraction added

--- Add.java
+++ Add.java
@@ -5,11 +5,15 @@ public class Add {
     public static int add(int firstNo, int secondNo){
         return firstNo+secondNo;
     }
+    public static int sub(int firstNo, int secondNo){
+        return firstNo-secondNo;
+    }
     public static void main(String args[])
     {   int firstNo=5,secondNo=3;
         int res=add(firstNo,secondNo);
-
+        int res1=sub(firstNo,secondNo);
         System.out.println(res);
+        System.out.println(res1);
 
     }
 }
