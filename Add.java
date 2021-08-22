dd985d9bff55fd272135c499af4aa02c1b75426d
renuka <renukadevi29730@gmail.com>
2021-08-22 01:32:04 -0700

    division is added

--- Add.java
+++ Add.java
@@ -11,14 +11,19 @@ public class Add {
     public static int mult(int firstNo, int secondNo){
         return firstNo*secondNo;
     }
+    public static int div(int firstNo, int secondNo){
+        return firstNo/secondNo;
+    }
     public static void main(String args[])
     {   int firstNo=5,secondNo=3;
         int res=add(firstNo,secondNo);
         int res1=sub(firstNo,secondNo);
         int res2=mult(firstNo,secondNo);
+        int res3=mult(firstNo,secondNo);
         System.out.println(res);
         System.out.println(res1);
         System.out.println(res2);
+        System.out.println(res3);
 
     }
 }
